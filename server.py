fe1a294d564bb3fe5b6016d9cef09ad584efc95a
Jun Kwan <kwanjunwen@gmail.com>
2020-10-03 14:59:48 +1000

    Add get_solar_insolation()

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
-import io
 import json
-
 from torchvision import models
 import torchvision.transforms as transforms
 from PIL import Image
@@ -12,6 +10,8 @@ import numpy as np
 import pandas as pd
 import os
 import requests
+import cv2 
+from skimage import io
 
 app = Flask(__name__)
 weights_pth = './nn_weight/mlp_weight.pth'
@@ -38,14 +38,7 @@ def get_temperature(lat_arr, lng_arr, datetime):
     # Define url for csv file containing land surface temperature
     nasa_url = 'https://neo.sci.gsfc.nasa.gov/archive/csv/MOD_LSTD_D/MOD_LSTD_D_{}.CSV.gz'.format(datetime)
 
-    # Retrive csv file using url
-    filename = nasa_url.split("/")[-1].split(".")[-3]
-
-    with open(filename, "wb") as f:
-        r = requests.get(nasa_url)
-        f.write(r.content)
-
-    df_temperature = pd.read_csv(filename, compression='gzip', header=None)
+    df_temperature = pd.read_csv(nasa_url, compression='gzip', header=None)
 
     # Rename index and column of the dataframe for the ease of accessing cell value
     df_temperature.columns = column_labels
@@ -54,8 +47,49 @@ def get_temperature(lat_arr, lng_arr, datetime):
     for lat, lng in list(zip(lat_arr, lng_arr)):
         temperature.append(df_temperature.loc[round(lat, 1), round(lng, 1)])
 
-    os.remove(filename)
     return temperature
+
+def get_solar_insolation(lat_arr, lng_arr, datetime):
+    solar_insolation = []
+
+    # Define column and index labels for renaming later
+    column_labels = np.around(np.arange(0.25,180.25,0.25), 2)
+    column_labels = np.concatenate((-np.flip(column_labels), column_labels))
+    index_labels = np.around(np.arange(0.25,90.25,0.25), 2)
+    index_labels = np.concatenate((np.flip(index_labels), -index_labels))
+
+    url = 'https://neo.sci.gsfc.nasa.gov/archive/rgb/CERES_INSOL_D/CERES_INSOL_D_{}.PNG'.format(datetime)
+  
+    # If url (or data) doesn't exist, then use the previous url (url_old). 
+    # Otherwise, update url_old with current url for future use
+    # request = requests.get(url)
+
+    # Retrive PNG using url
+    img = io.imread(url)
+
+    # Remove alpha channel of PNG image
+    if len(img.shape) > 2 and img.shape[2] == 4:
+        # Convert the image from RGBA2RGB
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+
+    # Compress 3D into 2D by computing the euclidean distance for each pixel
+    img_norm = np.linalg.norm(img, axis=2)
+
+    # Convert numpy array to data frame
+    df_solar_insolation = pd.DataFrame(data=img_norm)
+
+    # Rename index and column of the dataframe for the ease of accessing cell value
+    df_solar_insolation.columns = column_labels
+    df_solar_insolation.index = index_labels
+
+    for lat, long in list(zip(lat_arr, lng_arr)):
+        # Round latitude and longitude to nearest 0.25
+        lat_new = round(lat*4)/4
+        long_new = round(long*4)/4
+
+        solar_insolation.append(df_solar_insolation.loc[lat_new, long_new])
+    
+    return solar_insolation
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -80,6 +114,7 @@ def predict():
         lng_arr = np.arange(top_left_lng, bottom_right_lng, threshold)
 
         temperature = get_temperature(lat_arr,lng_arr,datetime)
+        solar_insolation = get_solar_insolation(lat_arr, lng_arr, datetime)
 
         params = np.array([29.015749, 325.264507747156, 0.668088614940643])
         class_id = get_prediction(in_vector=params)
